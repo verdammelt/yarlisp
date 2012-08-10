16780de9497b19fb3f67016a2e41ca203e92f186
Mark Simpson <verdammelt@gmail.com>
2012-08-10 19:48:31 -0400

    Starting to implement EVAL
    Handling atoms, and the following functions: QUOTE, ATOM, EQ

--- lib/yarlisp.rb
+++ lib/yarlisp.rb
@@ -11,7 +11,7 @@ module Yarlisp
     end
 
     def CAR(x)
-        raise('Undefined') if ATOM(x)
+        raise("Undefined") if ATOM(x)
         x[0]
     end
 
@@ -22,5 +22,37 @@ module Yarlisp
 
     def CONS(x, y)
         [x, y]
+    end
+
+    def EVAL(expr, env)
+        def equal(a, b)
+            if (ATOM a) && (ATOM b)
+                a.eql? b
+            elsif (equal (CAR a), (CAR b))
+                (equal (CDR a), (CDR b)) 
+            else
+                false
+            end
+        end
+        def assoc(x, a)
+            if (equal (CAR (CAR a)), x)
+                (CAR a)
+            else
+                (assoc x, (CDR a))
+            end
+        end
+
+        if (ATOM expr)
+            (CDR (assoc expr, env))
+        elsif (EQ (CAR expr), :QUOTE)
+            (CAR (CDR expr))
+        elsif (EQ (CAR expr), :ATOM)
+            (ATOM (EVAL (CAR (CDR expr)), env))
+        elsif (EQ (CAR expr), :EQ)
+            (EQ (EVAL (CAR (CDR expr)), env),
+             (EVAL (CAR (CDR (CDR expr))), env))
+        else
+            :NIL
+        end
     end
 end
--- spec/yarlisp_spec.rb
+++ spec/yarlisp_spec.rb
@@ -15,14 +15,14 @@ describe "YARLisp" do
     end
 
     it "car" do
-        (CAR [:x, :y]).should be :x
+        (CAR [:x, :y]).should eq :x
         (CAR [[:x, :y], :z]).should eq [:x, :y]
         expect { (CAR :x) }.to raise_error('Undefined')
     end
 
     it "cdr" do
-        (CDR [:x, :y]).should be :y
-        (CDR [[:x, :y], :z]).should be :z
+        (CDR [:x, :y]).should eq :y
+        (CDR [[:x, :y], :z]).should eq :z
         expect { (CDR :x) }.to raise_error('Undefined')
     end
 
@@ -32,10 +32,33 @@ describe "YARLisp" do
     end
 
     it "relationship of car, cdr and cons" do
-        (CAR(CONS :x, :y)).should be :x
-        (CDR(CONS :x, :y)).should be :y
+        (CAR(CONS :x, :y)).should eq :x
+        (CDR(CONS :x, :y)).should eq :y
 
         x = [:x, :y]
         (CONS(CAR(x), CDR(x))).should eq x
     end
+
+    context "eval" do
+        it "looks up atom in environment" do
+            (EVAL :x, [[:x, :y]]).should eq :y
+            (EVAL :x, [[:y, :a], [[:x, :b], :NIL]]).should eq :b
+        end
+
+        it "handles QUOTE function" do
+            (EVAL [:QUOTE, [:x, :NIL]], []).should eq :x
+        end
+
+        it "handles ATOM function" do
+            (EVAL [:ATOM, [:x, :NIL]], [[:x, :y]]).should be_true
+            (EVAL [:ATOM, [:x, :NIL]], [[:x, [:a, :b]]]).should_not be_true
+        end
+
+        it "handles EQ function" do
+            (EVAL [:EQ, [:x, [:y, :NIL]]], [[:x, :a], [[:y, :a], :NIL]]).should be_true
+            (EVAL [:EQ, [:x, [:y, :NIL]]], [[:x, :a], [[:y, :b], :NIL]]).should_not be_true
+        end
+    end
+
+
 end
