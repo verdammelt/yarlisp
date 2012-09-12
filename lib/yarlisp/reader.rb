dd4046985c3aa0d077c091bcf3755b8fe3df88be
Mark Simpson <verdammelt@gmail.com>
2012-09-11 21:26:24 -0400

    Cleaning up and correcting the initial tests for the reader - no real increase in functionality however

--- lib/yarlisp/reader.rb
+++ lib/yarlisp/reader.rb
@@ -1,7 +1,7 @@
 module Yarlisp
     module Reader
         def read str
-            []
+            str.to_sym
         end
     end
 end
--- spec/reader_spec.rb
+++ spec/reader_spec.rb
@@ -3,18 +3,24 @@ require 'yarlisp/reader'
 describe "the reader" do
     include Yarlisp::Reader
 
-    it "() => []" do
+    it "a => :a" do
+        read("a").should eq :a
+    end
+
+    xit "() => []" do
         read("()").should eq []
     end
 
     xit "(a) => [:a]" do
-        reader ("(a)").shoudl eq [:a]
+        reader ("(a)").should eq [:a]
     end
 
-    xit "(a b) => [:a, [:b, :nil]]" do
+    xit "(a b) => [:a, [:b, :NIL]]" do
+        reader("(a b)").should eq [:a, [:b, :NIL]]
     end
 
-    xit "((a) (b c)) => [[:a], [:b, [:c, :nil]]]" do
+    xit "((a) (b c)) => [[:a, :NIL], [[:b, [:c, :NIL]], :NIL]]" do
+        reader("((a) (b c))").should eq [[:a, :NIL], [[:b, [:c, :NIL]], :NIL]]
     end
 
 end
